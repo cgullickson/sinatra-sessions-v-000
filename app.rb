2d935b26e98aadd2d5a861039295b4f9e1b1c488
Chris Gullickson <cgullickson5@gmail.com>
2018-01-16 18:59:44 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -21,7 +21,7 @@ class App < Sinatra::Base
   get '/set' do
     # set the :foo key of the session hash equal to 'hello' here!
     session[:foo] = 'hello'
-
+    
   end
 
   get '/fetch' do
@@ -35,7 +35,12 @@ class App < Sinatra::Base
   get '/set_session' do
     #set session id here
     session[:id] == 1
-  
+    if session[:id] == 1
+      # "Session ID set. It's currently set to #{session[:id]}."
+      redirect '/fetch_session_id'
+    else
+      "Session ID has not been set!"
+    end
   end
 
   get '/fetch_session_id' do
